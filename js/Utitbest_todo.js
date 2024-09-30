f29060e4f75d96c0d2dc1a2a9e5bcd50236e7b3d
UtitBest <utitbesta@gmail.com>
2024-09-30 18:09:13 +0100

    bhy

--- js/Utitbest_todo.js
+++ js/Utitbest_todo.js
@@ -1,17 +1,43 @@
 let masters = document.querySelector('.masters')
 let Task_Name = document.querySelector('.Task_Name')
-let history = document.querySelector('.history')
-let botton = document.querySelector('.squad')
+let history1 = document.querySelector('.history1')
 let catapila = document.querySelector('.catapila')
+let know = document.querySelector('.know')
 
+
+
+function creator(){
+    let you = document.createElement('div');
+        you.className = 'know ghyy';
+        you.innerHTML = `
+            <div class="seems">
+                Enter task..... Tasks should not be empty.
+            </div>
+        `;
+        catapila.append(you)
+        clickingToAddTask()
+}
+creator()
+
+function clickingToAddTask(){
+    let botton = document.querySelector('.squad')
     botton.addEventListener('click', function(){
+        Task_Name = document.querySelector('.Task_Name');
+        know = document.querySelector('.know')
         if(Task_Name.value == ''){
-
+            know.classList.add('howwwo')
+            Task_Name.focus()
+            setInterval(() =>{
+                know.classList.remove('howwwo')
+            }, 4000)
+            return;
         }
-        HappeningFast()
+        HappeningFast(Task_Name.value)
+        let waking = Task_Name.value
+        localStorage.setItem('taskName', JSON.stringify(waking))
     });
-
-function HappeningFast(){
+}
+function HappeningFast(r){
     let blood = document.createElement('div');
         blood.className = 'sturbornss';
         blood.innerHTML = `
@@ -20,7 +46,7 @@ function HappeningFast(){
                     <input type="checkbox" name="" id="hername">
                 </div>
                 <div class="luv2">
-                    <p class="history"></p>
+                    <p class="history1">${r}</p>
                 </div>
                 <div class="luv3">
                     <div  class="broht">
@@ -32,14 +58,6 @@ function HappeningFast(){
         `;
         masters.append(blood)
 }
-function creator(){
-    let you = document.createElement('div');
-        you.className = 'know ghyy';
-        you.innerHTML = `
-            <div class="seems">
-                Enter task..... Tasks should not be empty.
-            </div>
-        `;
-        catapila.append(you)
-}
-creator()
+
+//  call this immediatly content load
+// window.addEventListener('DOMContentLoaded', HappeningFast())
